4e6404ed715383a0e8bc6aaf07238997159ff59c
Murad <muradmustafayev03@gmail.com>
2022-09-26 23:16:38 +0400

    regressors

---
+++ RegressionRecreation/Regressors.py
@@ -0,0 +1,14 @@
+class Regressor:
+    pass
+
+
+class LinearRegressor(Regressor):
+    pass
+
+
+class MultivariateRegressor(Regressor):
+    pass
+
+
+class PolynomialRegressor(Regressor):
+    pass
