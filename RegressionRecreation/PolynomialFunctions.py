b723377ef7e7f50739dc0a4428be101f4fcc8278
Murad <muradmustafayev03@gmail.com>
2022-09-26 07:27:32 +0400

    LinearFunction & MultivariateFunction

--- RegressionRecreation/PolynomialFunctions.py
+++ RegressionRecreation/PolynomialFunctions.py
@@ -4,8 +4,26 @@ class Polynomial:
 
     def eval(self, x):
         n = len(self.coefficients)
-        return sum([self.coefficients[i] * x ^ i for i in range(n)])
+        return sum([self.coefficients[i] * x ** i for i in range(n)])
 
     def derivative(self, x):
         n = len(self.coefficients)
-        return sum([self.coefficients[i] * i * x ^ (i-1) for i in range(n)])
+        return sum([self.coefficients[i] * i * x ** (i-1) for i in range(n)])
+
+
+class LinearFunction(Polynomial):
+    def __init__(self, a, b):
+        super().__init__([b, a])
+
+
+class MultivariateFunction:
+    def __init__(self, a, coefficients):
+        self.a = a
+        self.coefficients = coefficients
+
+    def eval(self, x):
+        n = len(self.coefficients)
+        return self.a + sum([self.coefficients[i] * x[i] for i in range(n)])
+
+    def gradient(self, x):
+        return self.coefficients
