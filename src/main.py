6827a03f2582335c5dc5c2f50e74d612e07d1792
Murad <muradmustafayev03@gmail.com>
2022-10-01 00:22:01 +0400

    ...

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ import numpy as np
 
 x = np.arange(10)
 y = np.arange(10) * 1.5 + 8
-# w = NormalEquation(np.matrix(x), np.matrix(y))
-# print(w)
 r = PolynomialRegressor(1)
 r.fit(x, y)
 print(r.predict(np.arange(10, 15)))
