b27225f5b5664907147c57c621c0c0b4fe5047e4
Murad <muradmustafayev03@gmail.com>
2022-09-30 06:50:55 +0400

    ...

--- RegressionRecreation/Regressors.py
+++ RegressionRecreation/Regressors.py
@@ -1,16 +1,7 @@
-from PolynomialFunctions import Polynomial, MultivariateFunction
 from optimisation_algorithms.GradientDescent import BatchGradientDescent
+from PolynomialFunctions import Polynomial, MultivariateFunction
+from utitilties import NormalEquation
 import numpy as np
-
-
-def MSE(hypothesis, x, y):
-    m = len(y)
-    return sum([(hypothesis(x[i]) - y[i]) ** 2 for i in range(m)]) / (2 * m)
-
-
-def NormalEquation(X: np.matrix, y: np.matrix):
-    X = np.c_[np.ones(len(X)), X]
-    return (np.linalg.inv(X.transpose().dot(X)).dot(X.transpose())).dot(y.transpose())
 
 
 class Regressor:
